89dcea8b11009698a8b929f3bd55468471145298
t3chy <elam.day.friedland@gmail.com>
2020-08-30 16:14:46 -0700

    broken run in debugger

--- hashmap.c
+++ hashmap.c
@@ -9,9 +9,9 @@ struct node {
     struct node*next;
 };
 typedef struct node node_t;
-node_t * initalize(int size) {
-    static node_t map[50];
-    for (int i=0; i < 49; i++) {
+node_t * initalize() {
+    static node_t map[capacity];
+    for (int i=0; i < capacity - 1; i++) {
         map[i].value = "none";
         map[i].next = NULL;
     }
@@ -25,36 +25,36 @@ int hash(char *key) {
         hashsum = hashsum % capacity;
     }
     hashsum = hashsum % capacity;
-    printf("hash for %s is %d \n",key,hashsum);
     return hashsum;
 
 }
 char* find(char *key, node_t map[]) {
     int hashval = hash(key);
+
     if (map[hashval].value == "none") {
-        printf("there is nothing here lmao\n");
+   //     printf("there is nothing here lmao\n");
         return "none";
     }
     else {
         if (map[hashval].key == key) {
-                printf("Item found! Value is: %s \n", map[hashval].value);
+             //   printf("Item found! Value is: %s \n", map[hashval].value);
                 return map[hashval].value;
         } else {
             node_t * temp = &map[hashval];
             while (temp -> next != NULL) {
                 if (temp -> key == key) {
-                printf("Item found! Value is: %s \n", temp -> value);
+           //     printf("Item found! Value is: %s \n", temp -> value);
                 return temp -> value;                    
                 }
                 temp = temp -> next;
             }
         if (temp -> key == key) {
-            printf("Item found! Value is: %s \n", temp -> value);
+          //  printf("Item found! Value is: %s \n", temp -> value);
             return temp -> value;                    
             }
         }
-        printf("something messed up lmao\n");
-        return "none";
+      //  printf("something messed up lmao\n");
+        return "fail";
     }
 }
 int add(node_t map[], char *key, char *value) {
@@ -95,6 +95,7 @@ void printList(struct node *temp)
   } 
 } 
 void writetheory(struct node booktowrite[]) {
+    printf("called");
     FILE * fp; 
     fp = fopen ("bookz", "wb");
     if (fp != NULL) {
@@ -105,27 +106,57 @@ void writetheory(struct node booktowrite[]) {
         printf("something went wrong opening the file\n");
 }
 }
-void readtheory(){
+node_t * readtheory(){
     FILE * fp; 
-    struct node mybook[capacity];
+    static node_t mybooks[capacity];
      fp = fopen ("bookz", "rb");
-    printf("file opened \n");
+   // printf("file opened \n");
     if (fp != NULL) {
-        fread(&mybook, sizeof(struct node), capacity, fp);
+        fread(&mybooks, sizeof(struct node), capacity, fp);
         fclose(fp);
-        printf("file opened\n");
-        printf("book title is %s",mybook[hash("Elam")].value);
+   //     printf("file opened\n");
+       // printf("book title is %s",mybooks[hash("Elam")].value);
     } else {
         printf("something went wrong opening the file\n");
-}  
+}  return mybooks;
 }
 int main() {
-    node_t * map = initalize(50);
-    add(map, "Elam", "ganer");
-    add(map, "not elam", "not gamrt");
-    add(map, "d elam", "not d");
-    find("d elam",map);
-    writetheory(map);
-    readtheory();
+    // node_t * map = initalize(50);
+    // add(map, "Elam", "ganer");
+    // add(map, "not elam", "not gamrt");
+    // add(map, "d elam", "not d");
+    // find("d elam",map);
+    // writetheory(map);
+   node_t * library = readtheory();
+   printf("  _      _ _                          \n | |    (_) |                         \n | |     _| |__  _ __ __ _ _ __ _   _ \n | |    | | '_ \\| '__/ _` | '__| | | |\n | |____| | |_) | | | (_| | |  | |_| |\n |______|_|_.__/|_|  \\__,_|_|   \\__, |\n                                 __/ |\n                                |___/ \n");
+   printf("What would you like to do?\n");
+   printf("[G]ive a book    --    [R]ead a Book! \n");
+   char selection; 
+   scanf("%c",&selection);
+   if (selection == 'G' || selection == 'g') {
+       printf("Great! What is it's title?\n");
+       char* title;
+       char* content;
+       scanf(" %s", &title);
+       printf("title: %s", title);
+       printf("paste the book here\n");
+       scanf(" %s", &content);
+       add(library,title,content);
+       printf("Thank you for your donation :) \n");
+       writetheory(library);
+    //   main();
+   } else {
+       char*ftitle;
+       printf("Please enter the title of the book you'd like to read: \n");
+       scanf("%s",ftitle);
+     //  printf("gamer %s", ftitle);
+       find(ftitle,library);
+       char* book;
+       if (book != (NULL) ) {
+           printf("here is your book! \n");
+       } else {
+           printf("sorry :( we don't have your book");
+       }
+   }
   return 0;
 }
